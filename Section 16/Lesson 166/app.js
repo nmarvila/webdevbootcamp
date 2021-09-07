ac9d7f6ffc01b889bd813eacaf0844bb21ed8d7f
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-09-07 16:47:50 -0300

    Committing Section 16

---
+++ Section 16/Lesson 166/app.js
@@ -0,0 +1,37 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//     console.log(random);
+// }
+
+
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//     console.log(random);
+// }
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log('UGHHH I HATE MONDAYS!');
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log('YAY I LOVE SATURDAYS!');
+// } else if (dayOfWeek === 'Friday') {
+//     console.log('FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!');
+// }
+
+
+// 0-5 - FREE
+// 5 - 10 CHILD $10
+// 10 - 65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 3;
+
+if (age < 5) {
+    console.log("You are a baby. You get in from free!");
+} else if (age < 10) {
+    console.log("You are a child. You pay $10");
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20");
+}
