06dd43a0c52cd03ff7a39f574efbfdb19571a90b
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-09-08 18:06:53 -0300

    Committing Lessons 191-201

---
+++ Section 19/Lesson 192/app.js
@@ -0,0 +1,26 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for (let num = 1; num <= 10; num += 1) {
+//     console.log(num);
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
