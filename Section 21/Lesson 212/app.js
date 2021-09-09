2e7891d6ac7aa2dbcc494fc2daea81456a8a8345
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-09-09 12:23:26 -0300

    Committing Section 21

---
+++ Section 21/Lesson 212/app.js
@@ -0,0 +1,44 @@
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!';
+// }
+
+// console.log(radius);
+// console.log(msg);
+
+// for (let i = 0; i < 5; i++) {
+//     let msg = 'kldfsajlkçdfs';
+//     console.log(msg);
+// }
+// console.log(msg);
+// console.log(i);
+
+function bankRobbery() {
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman'];
+    function cryForHelp() {
+        let color = 'purple';
+        function inner() {
+            for (let hero of heroes) {
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
+            }
+        }
+        inner();
+    }
+    cryForHelp();
+}
