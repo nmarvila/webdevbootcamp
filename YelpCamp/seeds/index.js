632238fc2214b94bd027c8dfd8edad7d9aa04ba9
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-09-27 12:28:13 -0300

    Committing Section 54

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -26,9 +26,18 @@ const seedDB = async () => {
             author: '614dd12a5bffa111c191b9cd',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, ratione. Placeat aliquam eos fuga laborum cumque cum, eligendi recusandae eveniet in. Nostrum sit, voluptate laborum dolor laboriosam sint voluptatem ipsa?',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/nmarvila/image/upload/v1632750572/YelpCamp/nmapridrtckafoslgxcl.png',
+                    filename: 'YelpCamp/nmapridrtckafoslgxcl'
+                },
+                {
+                    url: 'https://res.cloudinary.com/nmarvila/image/upload/v1632750572/YelpCamp/xqpsmqp65c0bcouqd3i6.png',
+                    filename: 'YelpCamp/xqpsmqp65c0bcouqd3i6'
+                }
+            ]
         })
         await camp.save();
     }
